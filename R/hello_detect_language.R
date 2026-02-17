9b27a8cbebf7208cb7781fdc7d6626979d4a6d73
wolfmissotten <wolfmissotten@gmail.com>
2026-02-17 14:29:57 +0100

    Update hello_detect_language.R
    Added an assert_that() line

--- R/hello_detect_language.R
+++ R/hello_detect_language.R
@@ -12,6 +12,7 @@
 #' hello_detect_language('hi')
 hello_detect_language <- function(greeting) {
   assertthat::assert_that(is.character(greeting), msg = "Greeting must be a character.")
+  assertthat::assert_that(length(greeting) == 1, msg = "Greeting must be a single character string.")
   for (language in names(dictionary)) {
     if (greeting %in% dictionary[[language]]) {
       return(language)}
